462642cee4c4051951308b1406e590d8a3f61679
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-02 14:57:51 +0700

    Added IUserDBHandler interface.

---
+++ src/persistent/IUserDBHandler.ts
@@ -0,0 +1,6 @@
+import IDBHandler from "./IDBHandler";
+import { UserData } from "./dtos/UserData";
+
+export default interface IUserDBHandler extends IDBHandler<UserData> {
+    get(username: string): Promise<UserData | undefined>;
+}
