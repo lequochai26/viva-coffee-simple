61dc92d13d735977319988753fb25f3700bbb267
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-02 15:16:34 +0700

    Added UserDBHandler class.

---
+++ src/persistent/UserDBHandler.ts
@@ -0,0 +1,88 @@
+import { get, getAll, getByFilter, insert, remove, update } from "./DBHandler";
+import IUserDBHandler from "./IUserDBHandler";
+import { UserData, userDataPattern } from "./dtos/UserData";
+
+// Collection info:
+const collectionName: string = "User";
+
+// Pattern:
+const pattern: UserData = userDataPattern;
+
+// Class definition
+class UserDBHandler implements IUserDBHandler {
+    // Constructor:
+    public constructor() {
+
+    }
+
+    // Methods:
+    public async getAll(): Promise<UserData[]> {
+        try {
+            return getAll(collectionName, pattern);
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async getByFilter(filter: any): Promise<UserData[]> {
+        try {
+            return getByFilter(collectionName, filter, pattern);
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async get(username: string): Promise<UserData | undefined> {
+        try {
+            return get(collectionName, { username: username }, pattern);
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async insert(userData: UserData): Promise<void> {
+        try {
+            return insert(collectionName, userData);
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async update(userData: UserData): Promise<void> {
+        try {
+            return update(collectionName, userData, { username: userData.username });
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async save(userData: UserData): Promise<void> {
+        try {
+            if (!await this.get(userData.username)) {
+                return this.insert(userData);
+            }
+            else {
+                return this.update(userData);
+            }
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async remove(userData: UserData): Promise<void> {
+        try {
+            return remove(collectionName, { username: userData.username });
+        }
+        catch (error: any) {
+            throw error;
+        }
+    }
+}
+
+export default UserDBHandler;
