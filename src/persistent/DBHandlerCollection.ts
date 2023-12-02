34c1a0870b5f8a30fa5f6ad7ab70ba2e3cba1b68
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-02 18:40:36 +0700

    Added OrderItemDBHandler instance into DBHandlerCollection module.

--- src/persistent/DBHandlerCollection.ts
+++ src/persistent/DBHandlerCollection.ts
@@ -1,10 +1,12 @@
 import IItemDBHandler from "./interfaces/IItemDBHandler";
 import IItemTypeDBHandler from "./interfaces/IItemTypeDBHandler";
 import IOrderDBHandler from "./interfaces/IOrderDBHandler";
+import IOrderItemDBHandler from "./interfaces/IOrderItemDBHandler";
 import IUserDBHandler from "./interfaces/IUserDBHandler";
 import ItemDBHandler from "./ItemDBHandler";
 import ItemTypeDBHandler from "./ItemTypeDBHandler";
 import OrderDBHandler from "./OrderDBHandler";
+import OrderItemDBHandler from "./OrderItemDBHandler";
 import UserDBHandler from "./UserDBHandler";
 
 // UserDBHandler
@@ -12,3 +14,4 @@ export const userDBHandler: IUserDBHandler = new UserDBHandler();
 export const itemDBHandler: IItemDBHandler = new ItemDBHandler();
 export const itemTypeDBHandler: IItemTypeDBHandler = new ItemTypeDBHandler();
 export const orderDBHandler: IOrderDBHandler = new OrderDBHandler();
+export const orderItemDBHandler: IOrderItemDBHandler = new OrderItemDBHandler();
