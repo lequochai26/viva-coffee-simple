b29c2e43d9b460875805f956ee127a7a5de17fca
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-03 01:34:42 +0700

    Renamed IUserEntityManager interface to IUserManager.

--- src/domain/interfaces/IUserManager.ts
+++ src/domain/interfaces/IUserManager.ts
@@ -1,6 +1,6 @@
 import User from "../entities/User";
 import EntityManager from "./EntityManager";
 
-export default interface IUserEntityManager extends EntityManager<User> {
+export default interface IUserManager extends EntityManager<User> {
     get(username: string): Promise<User | undefined>
 }
