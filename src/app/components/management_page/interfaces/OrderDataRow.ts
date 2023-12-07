293202bb3f5a4e399668b1d9b32f09284e08652c
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-08 03:11:31 +0700

    Added OrderDataRow.

---
+++ src/app/components/management_page/interfaces/OrderDataRow.ts
@@ -0,0 +1,6 @@
+import { Order } from "@/app/interfaces/Order";
+import DataRow from "../../management_panel/interfaces/DataRow";
+
+export default interface OrderDataRow extends Order, DataRow {
+    
+}
