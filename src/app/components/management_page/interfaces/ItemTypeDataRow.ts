6bf2a023364363c6d1c983639c069f7626a56fc0
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-07 11:56:51 +0700

    Added ItemTypeDataRow interface.

---
+++ src/app/components/management_page/interfaces/ItemTypeDataRow.ts
@@ -0,0 +1,6 @@
+import ItemType from "@/app/interfaces/ItemType";
+import DataRow from "../../management_panel/interfaces/DataRow";
+
+export default interface ItemTypeDataRow extends ItemType, DataRow {
+    
+}
