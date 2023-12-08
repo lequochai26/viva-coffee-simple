e5e85086be82b449c5f9e4b64a5d609d350756f9
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-09 01:44:02 +0700

    Added load item list feature for AddOrderScreen's AddOrderItemBox component.

--- src/app/components/management_page/add_order_screen/AddOrderScreen.tsx
+++ src/app/components/management_page/add_order_screen/AddOrderScreen.tsx
@@ -1,10 +1,14 @@
 import { Order } from "@/app/interfaces/Order";
 import EntityAlterScreenProps from "../../management_panel/interfaces/EntityAlterScreenProps";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import User from "@/app/interfaces/User";
 import Item from "@/app/interfaces/Item";
 import AddOrderBox from "./AddOrderBox";
 import AddOrderItemBox from "./AddOrderItemBox";
+
+// Info:
+const routeHandler: string = "/management/order";
+const itemManagementRouteHandler: string = "/management/item";
 
 // Main component:
 export default function AddOrderScreen({ user, onAlter, close }: EntityAlterScreenProps<Order>) {
@@ -42,6 +46,47 @@ export default function AddOrderScreen({ user, onAlter, close }: EntityAlterScre
     );
     const [ keyword, setKeyword ] = useState<string>("");
 
+    // Data operations:
+    async function load(keyword?: string): Promise<void> {
+        try {
+            // Sending HTTP request and receiving response
+            const response: Response = await fetch(
+                (keyword ? `${itemManagementRouteHandler}?keyword=${keyword}` : itemManagementRouteHandler),
+                {
+                    method: "GET",
+                    headers: [
+                        ["method", "GETALL"]
+                    ]
+                }
+            );
+
+            // Parsing response's body into json
+            const { success, result, message }: { success: boolean, result: Item[], message: string } = await response.json();
+
+            // Failed case
+            if (!success) {
+                alert(message);
+            }
+            // Success case
+            else {
+                setItemList(
+                    itemsToItemList(result)
+                );
+            }
+        }
+        catch (error: any) {
+            alert("Đã có lỗi xảy ra!");
+            console.error(error);
+        }
+    }
+
+    // Effects:
+    useEffect(
+        function () {
+            load();
+        }, []
+    )
+
     // View:
     return (
         <div className="inlineBlock verticalAlignMiddle widthFitParent heightFitContent">
@@ -65,4 +110,23 @@ export default function AddOrderScreen({ user, onAlter, close }: EntityAlterScre
             />
         </div>
     )
+}
+
+// Local functions:
+function itemsToItemList(items: Item[]): { [index: string]: Item[] } {
+    // Item list initialization
+    const itemList: { [index: string]: Item[] } = {};
+
+    // Conversion
+    items.forEach(
+        function (item: Item): void {
+            if (!itemList[item.typeName]) {
+                itemList[item.typeName] = [];
+            }
+            itemList[item.typeName].push(item);
+        }
+    )
+
+    // Return item list
+    return itemList
 }
