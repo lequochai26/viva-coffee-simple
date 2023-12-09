2acb4d61cb943adb9072d9c4dc7d54f9f876c23a
Lê Quốc Hải <lequochai26@gmail.com>
2023-12-09 13:00:44 +0700

    Added DeleteConfirmDialog prop for Management Panel.

--- src/app/components/management_panel/ManagementPanel.tsx
+++ src/app/components/management_panel/ManagementPanel.tsx
@@ -5,6 +5,7 @@ import DataRow from "./interfaces/DataRow";
 import DataTable from "./data_table/DataTable";
 import User from "@/app/interfaces/User";
 import EntityAlterScreenProps from "./interfaces/EntityAlterScreenProps";
+import ConfirmDialogProps from "./interfaces/ConfirmDialogProps";
 
 // Interfaces:
 interface ManagementPanelProps<T> {
@@ -20,11 +21,12 @@ interface ManagementPanelProps<T> {
     AddScreen?(props: EntityAlterScreenProps<T>): JSX.Element;
     EditScreen?(props: EntityAlterScreenProps<T>): JSX.Element;
     ViewScreen?(props: EntityAlterScreenProps<T>): JSX.Element;
+    DeleteConfirmDialog(props: ConfirmDialogProps): JSX.Element
     user: User;
 }
 
 // Main component:
-export default function ManagementPanel<T extends DataRow>({ config, AddScreen, EditScreen, ViewScreen, user }: ManagementPanelProps<T>) {
+export default function ManagementPanel<T extends DataRow>({ config, AddScreen, EditScreen, ViewScreen, DeleteConfirmDialog, user }: ManagementPanelProps<T>) {
     // States:
     const [ fixedScreenContent, setFixedScreenContent ] = useState<JSX.Element | undefined>(undefined);
     const [ data, setData ] = useState<T[]>([]);
@@ -218,6 +220,24 @@ export default function ManagementPanel<T extends DataRow>({ config, AddScreen, 
         );
     }
 
+    function showDeleteConfirmDialog(index: number): void {
+        setFixedScreenContent(
+            <DeleteConfirmDialog
+                onCancel={closeFixedScreen}
+                onConfirm={function () {deleteRow(index)}}
+            />
+        )
+    }
+
+    function showDeleteSelectedConfirmDialog(): void {
+        setFixedScreenContent(
+            <DeleteConfirmDialog
+                onCancel={closeFixedScreen}
+                onConfirm={deleteSelected}
+            />
+        )
+    }
+
     // Event handlers:
     function onSearchKeywordChange({ target }: any): void {
         setSearchKeyword(target.value);
@@ -266,10 +286,30 @@ export default function ManagementPanel<T extends DataRow>({ config, AddScreen, 
             {/* Management panel */}
             <div className="block widthFitParent heightFitParent">
                 {/* Header */}
-                <Header deletable={config.deletable} keyword={searchKeyword} onAdd={showAddScreen} onDelete={deleteSelected} onReload={load} onKeywordChange={onSearchKeywordChange} onSearch={loadByKeyword} />
+                <Header
+                    deletable={config.deletable}
+                    keyword={searchKeyword}
+                    onAdd={showAddScreen}
+                    onDelete={showDeleteSelectedConfirmDialog}
+                    onReload={load}
+                    onKeywordChange={onSearchKeywordChange}
+                    onSearch={loadByKeyword}
+                />
 
                 {/* Table */}
-                <DataTable data={data} selectAll={selectAll} tableConfig={config.table} editable={config.editable} viewable={config.viewable} deletable={config.deletable} onSelectAll={onSelectAll} onSelect={onRowSelect} onDelete={deleteRow} onEdit={showEditScreen} onView={showViewScreen} />
+                <DataTable
+                    data={data}
+                    selectAll={selectAll}
+                    tableConfig={config.table}
+                    editable={config.editable}
+                    viewable={config.viewable}
+                    deletable={config.deletable}
+                    onSelectAll={onSelectAll}
+                    onSelect={onRowSelect}
+                    onDelete={showDeleteConfirmDialog}
+                    onEdit={showEditScreen}
+                    onView={showViewScreen}
+                />
             </div>
         </>
     );
